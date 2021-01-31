70dac3f2a6e33205a4c23ac4a902323bb41083c4
ThatGuyDed <radgekid@hotmail.co.uk>
2021-01-31 22:59:08 +0000

    Fixed issues with recurring events
    God damn it east why did you have to make such a cool weekly league play, it broke my command >:(

--- RLBotDiscordBot/calendars.py
+++ RLBotDiscordBot/calendars.py
@@ -30,7 +30,7 @@ async def checkCalendar(message):
             name, some_time, time_until = date_time_check(today, jsons, 0)
             tournaments_embed = discord.Embed(
                         title=name,
-                        description=f"Will begin in {time_until}. ({some_time})" ,
+                        description=f"Will begin in {time_until}. ({some_time})" , #after even has started but not finished, this will say "Will being in {time} ago" I am not sure how to fix this using the API arguments
                         color=discord.Color.green()
                         )
             tournaments_embed.set_author(name="Upcoming Tournaments", icon_url="https://cdn.discordapp.com/avatars/474703464199356447/720a25621983c452cf71422a51b733a1.png?size=128")
@@ -56,6 +56,22 @@ def date_time_check(today, jsons, num):
     names = jsons["items"][num]["summary"]
     start = jsons["items"][num]["start"]["dateTime"]
     new_date = datetime.datetime.strptime(start, FORMAT)
+    try:
+        recurrence = jsons["items"][num]["recurrence"][0].split(";")
+        rec_type = recurrence[0].split("=")[1]
+        num_recs = recurrence[2].split("=")[1]
+        if rec_type == "WEEKLY":
+            for i in range(int(num_recs)):
+                if new_date > today:
+                    break
+                new_date += datetime.timedelta(days=7)
+        elif rec_type == "MONTHLY":
+            for i in range(int(num_recs)):
+                if new_date > today:
+                    break
+                new_date += datetime.timedelta(months=1)
+    except:
+        pass
     some_times = new_date.strftime(FORMAT2)
     time_untils = date.duration(new_date, now=today, precision=3)
     return names, some_times, time_untils
